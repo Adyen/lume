e429ae037aff8139fd25ff5375b146f38b9d41c7
Lucas van Heerikhuizen <lucas.vanheerikhuizen@adyen.com>
2022-04-06 16:27:18 +0200

    Forgot to populate the bar-group.vue component

---
+++ src/bar-chart/bar-group.vue
@@ -0,0 +1,34 @@
+<template>
+  <g>
+    <bar v-bind="bar" />
+    <bar
+        v-if="overlay"
+        v-bind="overlay"
+        :fill="`rgba(0, 0, 0, ${isHovered ? .3 : 0})`"
+        @mouseover.native="$emit('mouseover', $event)"
+        @mouseout.native="$emit('mouseout')"
+    />
+  </g>
+</template>
+
+<script>
+import Bar from './bar.vue';
+
+export default {
+  components: { Bar },
+  props: {
+    bar: {
+      type: Object,
+      required: true
+    },
+    overlay: {
+      type: Object,
+      default: null
+    },
+    isHovered: {
+      type: Boolean,
+      required: true
+    }
+  }
+}
+</script>
