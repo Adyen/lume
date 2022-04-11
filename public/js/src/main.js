06577b32617debafafffd86422a476c82a8d7561
govind <govind.srinidhi@adyen.com>
2022-04-11 15:56:01 +0200

    added d3 dependency

--- public/js/src/main.js
+++ public/js/src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
-import App from './components/app.vue';
+// import App from './components/app.vue';
+import App from '../../../src/box-plot/box-plot.vue'
 
 const vueApp = new Vue({
     el: document.getElementById('app'),
--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,7 @@ module.exports = {
                     plugins: [
                         '@babel/plugin-proposal-nullish-coalescing-operator',
                         '@babel/plugin-proposal-optional-chaining',
+                        '@babel/plugin-proposal-optional-chaining'
                     ]
                 }
             },
