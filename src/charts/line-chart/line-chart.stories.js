ba9fd1143cd66d0c91c20956f30b4fdc1fb72b6b
joaosa <joao.dossantos@adyen.com>
2022-05-05 11:15:25 +0200

    Add multiple datasets to line chart stories

--- src/charts/line-chart/line-chart.stories.js
+++ src/charts/line-chart/line-chart.stories.js
@@ -1,31 +1,50 @@
 import { storiesOf } from '@storybook/vue';
-import { boolean } from '@storybook/addon-knobs';
+import { boolean, select } from '@storybook/addon-knobs';
 import { SizeKnobsMixin } from '@/utils/storybook-helpers';
 import LineChart from './line-chart.vue';
 import notes from './README.md';
+
+const DATASETS = {
+    Simple: {
+        data: [
+            { values: [10, 30, -20, 50, 40, 70, 60], color: '01', legend: 'Hamburgers' },
+            { values: [30, 10, 20, 70, 50, null, 40], color: '02', legend: 'Hot dogs' },
+        ],
+        labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+    },
+    'Chargebacks & Fraud overview - 28 days': {
+        data: [{ "values": [0.0036, 0.004, 0.0036, 0.0036, 0.0036, 0.0036, 0.0038, 0.0036, 0.0036, 0.0037, 0.0041, 0.0041, 0.0038, 0.0041, 0.004, 0.0036, 0.0036, 0.0041, 0.0041, 0.004, 0.0038, 0.0041, 0.0037, 0.0038, 0.0037, 0.0041, 0.0038, 0.0036], "legend": "Chargebacks", "color": "07", "sticky": true }, { "values": [0.0007, 0.0008, 0.0007, 0.0007, 0.0007, 0.0007, 0.0007, 0.0007, 0.0007, 0.0007, 0.0009, 0.0008, 0.0007, 0.0008, 0.0008, 0.0007, 0.0007, 0.0009, 0.0008, 0.0008, 0.0007, 0.0009, 0.0007, 0.0007, 0.0007, 0.0008, 0.0007, 0.0007], "legend": "NOFs", "color": "06", "sticky": true }],
+        labels: ["Apr 07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "May 01", "02", "03", "04"]
+    }
+}
 
 storiesOf('Charts / Line chart', module)
     .add('Basic', () => ({
         components: { LineChart },
         props: {
+            dataset: {
+                type: String,
+                default: select('Dataset', Object.keys(DATASETS), Object.keys(DATASETS)[0]),
+            },
             startOnZero: {
                 type: Boolean,
                 default: boolean('Start on zero', true)
             },
             ...SizeKnobsMixin(),
         },
+        computed: {
+            data() {
+                return DATASETS[this.dataset].data;
+            },
+            labels() {
+                return DATASETS[this.dataset].labels;
+            }
+        },
         template: `
             <div :style="{ width: width + 'px', height: height + 'px' }">
                 <line-chart
-                    :data="lineChartData" :labels="barChartLabels" :start-on-zero="startOnZero"
+                    :data="data" :labels="labels" :start-on-zero="startOnZero"
                 />
             </div>
         `,
-        data: () => ({
-            lineChartData: [
-                { values: [10, 30, -20, 50, 40, 70, 60], color: '01', legend: 'Hamburgers' },
-                { values: [30, 10, 20, 70, 50, null, 40], color: '02', legend: 'Hot dogs' },
-            ],
-            barChartLabels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-        })
     }), { notes });
