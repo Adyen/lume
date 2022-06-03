d7927d87848b5152eea6741a556d1d1d2f4c5f79
Lucas van Heerikhuizen <lucas.vanheerikhuizen@adyen.com>
2022-06-03 09:45:44 +0200

    FIX Added story for horizontal stacked bar chart and restored story for bar chart

--- src/charts/horizontal-grouped-bar-chart/horizontal-grouped-bar-chart.stories.js
+++ src/charts/horizontal-grouped-bar-chart/horizontal-grouped-bar-chart.stories.js
@@ -4,28 +4,33 @@ import {
   SizeKnobsMixin,
   convertAxisPropsIntoOptions,
 } from '@/utils/storybook-helpers';
-import BarChart from './bar-chart.vue';
+import HorizontalGroupedBarChart from './horizontal-grouped-bar-chart.vue';
 
-storiesOf('Charts / Bar chart', module) // eslint-disable-line
+storiesOf('Charts / Horizontal grouped bar chart', module) // eslint-disable-line
   .add('Basic', () => ({
-    components: { BarChart },
+    components: { HorizontalGroupedBarChart },
     props: {
-      ...AxisOptionsMixin('x'),
-      ...AxisOptionsMixin('y'),
+      ...AxisOptionsMixin('x', true),
+      ...AxisOptionsMixin('y', true),
       ...SizeKnobsMixin(),
     },
     template: `
             <div :style="{ width: width + 'px', height: height + 'px' }">
-                <bar-chart
+                <horizontal-grouped-bar-chart
                     :data="data" :labels="labels" :options="options"
                 />
             </div>
         `,
     data: () => ({
-      data:  [{
-        values: [10, 40, null, 30, 60, 0, -26],
-        color: '02'
-      }],
+      data: [
+        { values: [10, 20, 30] },
+        { values: [20, 30, 40] },
+        { values: [-10, 0, 30] },
+        { values: [-20, -30, 40] },
+        { values: [60, null, -50] },
+        { values: [40, -10, 30] },
+        { values: [-50, 30, 20] },
+      ],
       labels: [
         'Monday',
         'Tuesday',
