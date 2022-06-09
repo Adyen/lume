968329dbce88a7d85c5eb1ba34a4751bcbad2927
govind <govind.srinidhi@adyen.com>
2022-06-09 14:40:02 +0200

    VID-2618: updated the box plot props as per new dataset

--- public/js/src/components/app.vue
+++ public/js/src/components/app.vue
@@ -49,9 +49,7 @@
     <div class="row">
       <box-plot
         :data="boxPlotData"
-        :margins="boxPlotConfig.margins"
-        :group-by-label="boxPlotConfig.groupByLabel"
-        :value-label="boxPlotConfig.valueLabel"
+        :y-axis-options="{ label: 'Boxes' }"
       />
       <sparkline :data="barChartData" />
     </div>
@@ -114,7 +112,38 @@ export default defineComponent({
       'Sunday',
     ],
     barsConfig: { color: '03', legend: 'Hot dogs' },
-    boxPlotData,
+    boxPlotData: [
+      {
+        values: [5.1, 4.9, 4.7, 4.6, 5, 5.4, 4.6, 5, 4.4, 4.9, 5.4, 4.8, 4.8, 4.3, 5.8, 5.7],
+        color: '01',
+        legend: 'Type 1',
+      },
+      {
+        values: [5.4, 5.1, 5.7, 5.1, 5.4, 5.1, 4.6, 5.1, 4.8, 5, 5, 5.2, 5.2, 4.7, 4.8, 5.4, 5.2, 5.5, 4.9, 5, 5.5, 4.9, 4.4, 5.1, 5, 4.5, 4.4, 5, 5.1, 4.8, 5.1, 4.6, 5.3, 5],
+        color: '02',
+        legend: 'Type 2'
+      },
+      {
+        values: [7, 6.4, 6.9, 5.5, 6.5, 5.7, 6.3, 4.9, 6.6, 5.2, 5, 5.9, 6, 6.1, 5.6, 6.7],
+        color: '03',
+        legend: 'Type 3'
+      },
+      {
+        values: [5.6, 5.8, 6.2, 5.6, 5.9, 6.1, 6.3, 6.1, 6.4, 6.6, 6.8, 6.7, 6, 5.7, 5.5, 5.5, 5.8, 6, 5.4, 6, 6.7, 6.3, 5.6, 5.5, 5.5, 6.1, 5.8, 5, 5.6, 5.7, 5.7, 6.2, 5.1, 5.7],
+        color: '04',
+        legend: 'Type 4'
+      },
+      {
+        values: [6.3, 5.8, 7.1, 6.3, 6.5, 7.6, 4.9, 7.3, 6.7, 7.2, 6.5, 6.4, 6.8, 5.7, 5.8, 6.4, 6.5, 7.7, 7.7, 6, 6.9, 5.6, 7.7],
+        color: '05',
+        legend: 'Type 5'
+      },
+      {
+        values: [6.3, 6.7, 7.2, 6.2, 6.1, 6.4, 7.2, 7.4, 7.9, 6.4, 6.3, 6.1, 7.7, 6.3, 6.4, 6, 6.9, 6.7, 6.9, 5.8, 6.8, 6.7, 6.7, 6.3, 6.5, 6.2, 5.9],
+        color: '06',
+        legend: 'Type 6'
+      }
+    ],
     boxPlotConfig: {
       groupByLabel: 'Species',
       valueLabel: 'Sepal_Length',
