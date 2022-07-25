fd7e2a4ac3222a1357fb920d1d8634911b29570d
Lucas van Heerikhuizen <lucas.vanheerikhuizen@adyen.com>
2022-07-25 12:37:15 +0200

    Adding mock function quick and dirty alternative for structuredClone for Jest tests

--- test/config/setupTests.ts
+++ test/config/setupTests.ts
@@ -4,5 +4,6 @@ import Vue from 'vue';
 Vue.use(VueCompositionApi);
 
 global.ResizeObserver = require('resize-observer-polyfill');
+global.structuredClone = object => JSON.parse(JSON.stringify(object));
 
 export {}
