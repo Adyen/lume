a82cc3d67cc55d466113510b20b0feb804645fc3
joaosa <joao.dossantos@adyen.com>
2022-08-02 13:19:41 +0200

    💄 Improve Storybook preview styles

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,8 @@
-import Vue from 'vue';
+import Vue, { Component } from 'vue';
 import VueCompositionAPI from '@vue/composition-api';
 
 import '@/styles/main.scss';
+import './preview-styles.scss';
 
 if (process.env.NODE_ENV === 'development') {
   Vue.config.performance = true;
@@ -12,8 +13,34 @@ Vue.use(VueCompositionAPI);
 
 export const parameters = {
   passArgsFirst: false,
+  backgrounds: {
+    default: 'light',
+    values: [
+      {
+        name: 'light',
+        value: '#f3f6f9',
+      },
+      {
+        name: 'dark',
+        value: '#20304c',
+      },
+    ],
+  },
   controls: {
     hideNoControlsWarning: true,
     expanded: true, // show all documentation with control input
   },
 };
+
+export const decorators = [
+  (Story: Component) => ({
+    components: { Story },
+    template: `
+      <div class="preview">
+        <div class="card">
+          <story />
+        </div>
+      </div>
+    `,
+  }),
+];
