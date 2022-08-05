b3bc0311d72a74038d71a2f874de4f1dd3c8f505
Lucas van Heerikhuizen <lucas.vanheerikhuizen@adyen.com>
2022-08-05 16:45:12 +0200

    ✅ Added unit test for the adv-line.vue core component

---
+++ test/unit/specs/core/adv-line/adv-line.spec.ts
@@ -0,0 +1,31 @@
+import { mount } from '@vue/test-utils';
+import { data, xScale, yScale } from '../../mock-data';
+import Line from '@/core/adv-line';
+
+const propsData = { index: 0, values: data[0].values, xScale, yScale };
+
+describe('chart-container.vue', () => {
+    test('mounts component', () => {
+        const wrapper = mount(Line, { propsData })
+
+        const el = wrapper.find('[data-j-line]');
+        expect(el.exists()).toBeTruthy();
+        expect(el.classes().includes('adv-line--dashed')).toBe(false)
+    });
+
+    test('mounts component and sets dashed line to true', () => {
+        const wrapper = mount(Line, { propsData: { ...propsData, dashed: true } })
+
+        const el = wrapper.find('[data-j-line]');
+        expect(el.classes().includes('adv-line--dashed')).toBe(true)
+        expect(el.classes().includes(`adv-stroke-color--01`)).toBe(true)
+    });
+
+    test('mounts component with custom color', () => {
+        const color = '02';
+        const wrapper = mount(Line, { propsData: { ...propsData, color } })
+
+        const el = wrapper.find('[data-j-line]');
+        expect(el.classes().includes(`adv-stroke-color--${color}`)).toBe(true)
+    });
+});
