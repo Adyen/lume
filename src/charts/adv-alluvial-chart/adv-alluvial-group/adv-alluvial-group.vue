fd1df5315b79c1ca0bf6b42850bb423efa8ef18e
govind <govind.srinidhi@adyen.com>
2022-08-16 16:58:40 +0200

    🎨 applied BEM for alluvial group

--- src/charts/adv-alluvial-chart/adv-alluvial-group/adv-alluvial-group.vue
+++ src/charts/adv-alluvial-chart/adv-alluvial-group/adv-alluvial-group.vue
@@ -7,7 +7,7 @@
       v-for="(_, index) in alluvialInstance.nodeBlocks"
       :id="`${_.id}`"
       :key="`node-block_${index}`"
-      class="node"
+      class="adv-alluvial-group__node"
     >
       <rect
         :transform="`translate(${_.node.x0},${_.node.y0})`"
@@ -18,16 +18,16 @@
         @mouseout="() => alluvialInstance.highlightedNode = null"
       />
       <text
-        class="node__label"
+        class="adv-alluvial-group__node__label"
         :transform="`translate(${_.textTransform.x},${_.textTransform.y})`"
-        :class="{'node__label--right': _.node.depth === 0, 'node__label--color': _.node.depth === maxDepth }"
+        :class="{'adv-alluvial-group__node__label--right': _.node.depth === 0, 'adv-alluvial-group__node__label--color': _.node.depth === maxDepth }"
       >
         <tspan
-          class="node__label__title"
+          class="adv-alluvial-group__node__label__title"
           v-text="_.node.label"
         />
         <tspan
-          class="node__label__value"
+          class="adv-alluvial-group__node__label__value"
           x="0"
           dy="1.2em"
           v-text="dataWithDefaults.valueFormatter(_.node.value)"
@@ -37,13 +37,13 @@
     <g
       v-for="(_, index) in alluvialInstance.linkPaths"
       :key="`link-path_${index}`"
-      class="path-group"
+      class="adv-alluvial-group__path-group"
     >
       <g style="mix-blend-mode: multiply">
         <path
           :id="_.id"
           :d="_.d"
-          :class="[`path-group__link--${_.color}`, 'path-group__link']"
+          :class="[`adv-alluvial-group__path-group__link--${_.color}`, 'adv-alluvial-group__path-group__link']"
           :stroke-width="_.strokeWidth"
           @mouseover="() => alluvialInstance.highlightedLink = _.link"
           @mouseout="() => alluvialInstance.highlightedLink = null"
@@ -57,12 +57,13 @@
 import {defineComponent, Ref, ref, toRefs, watch} from '@vue/composition-api';
 import {defaultProps, nodeToLabelGap} from '@/charts/adv-alluvial-chart/defaults';
 import { AlluvialInstance } from '@/types/alluvial';
-import {useBase, useDefaults} from '@/charts/adv-alluvial-chart/mixins/base';
-import { useBase as prepareBase } from '@/mixins/base';
-import { drawPlot } from '@/charts/adv-alluvial-chart/mixins/plot';
+import { prepareAlluvialBuildingBlocks, useDefaults } from '@/charts/adv-alluvial-chart/mixins/adv-alluvial-building-blocks';
+import { useBase } from '@/mixins/base';
+import { drawPlot } from '@/charts/adv-alluvial-chart/mixins/adv-alluvial-interactions';
 import { withChartProps } from '@/mixins/props';
 import {singleDatasetValidator} from '@/utils/helpers';
 import { ContainerSize } from '@/types/size';
+import { determineCoordinates } from '../mixins/adv-alluvial-coordinates';
 
 
 export default defineComponent({
@@ -72,7 +73,7 @@ export default defineComponent({
   setup(props, context) {
     const chartContainer = ref(null);
     const { data } = toRefs(props);
-    const { computedData } = prepareBase(data);
+    const { computedData } = useBase(data);
     const dataWithDefaults = useDefaults(computedData.value[0], defaultProps);
 
     const alluvialData: Ref<AlluvialInstance> = ref({
@@ -86,12 +87,16 @@ export default defineComponent({
       nodeBlocks: [],
       linkPaths: []
     });
-    const { graph, nodeId, alluvialInstance } = useBase(dataWithDefaults, alluvialData);
+    const { graph, nodeId, alluvialInstance } = prepareAlluvialBuildingBlocks(dataWithDefaults, alluvialData);
+
     const {
       leftMostNodeLabelWidth,
       rightMostNodeLabelWidth,
       topMostNodeLabelExtraHeight,
-      bottomMostNodeLabelExtraHeight,
+      bottomMostNodeLabelExtraHeight
+    } = determineCoordinates(dataWithDefaults, graph, chartContainer, nodeId);
+
+    const {
       highlightedElements,
       highlightLinks,
       updateNodes,
@@ -115,9 +120,7 @@ export default defineComponent({
     watch(rightMostNodeLabelWidth, width => alluvialInstance.value.rightExtent = alluvialInstance.value.containerSize.width - (width + nodeToLabelGap));
     watch(bottomMostNodeLabelExtraHeight, height => alluvialInstance.value.bottomExtent = height);
     watch(topMostNodeLabelExtraHeight, height => alluvialInstance.value.topExtent = height);
-    watch(alluvialInstance.value.containerSize, (containerSize: ContainerSize) => {
-      alluvialInstance.value.rightExtent = containerSize.width - (rightMostNodeLabelWidth.value + nodeToLabelGap)
-    });
+    watch(alluvialInstance.value.containerSize, containerSize => alluvialInstance.value.rightExtent = containerSize.width - (rightMostNodeLabelWidth.value + nodeToLabelGap));
 
     return {
       chartContainer,
