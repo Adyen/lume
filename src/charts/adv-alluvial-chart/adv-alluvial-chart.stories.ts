29c3dbf2428d2284ed2975ce1d0c726eac571747
govind <govind.srinidhi@adyen.com>
2022-08-16 17:47:31 +0200

    📝 added id to each of the alluvial node in the alluvial story

--- src/charts/adv-alluvial-chart/adv-alluvial-chart.stories.ts
+++ src/charts/adv-alluvial-chart/adv-alluvial-chart.stories.ts
@@ -1,12 +1,13 @@
 import { withSizeArgs, withSizeArgTypes } from '@/utils/storybook-helpers';
-import AlluvialChart from './alluvial-chart.vue';
+import AlluvialChart from './adv-alluvial-chart.vue';
 
-const alluvial = {
+const alluvial = [{
     values: [
         {
             label: 'A',
             color: '01',
             value: 'A',
+            id: 'A',
             targets: [
                 { node: 'D', value: 15 },
                 { node: 'E', value: 42 },
@@ -16,6 +17,7 @@ const alluvial = {
             label: 'B',
             color: '02',
             value: 'B',
+            id: 'B',
             targets: [
                 { node: 'D', value: 45 },
                 { node: 'E', value: 42 },
@@ -25,6 +27,7 @@ const alluvial = {
             label: 'C',
             color: '03',
             value: 'C',
+            id: 'C',
             targets: [
                 { node: 'D', value: 20 },
             ],
@@ -32,17 +35,19 @@ const alluvial = {
         {
             label: 'D',
             value: 'D',
+            id: 'D',
             color: '07',
         },
         {
             label: 'E',
             value: 'E',
+            id: 'E',
             color: '06',
         },
     ],
     nodePadding: 20,
     nodeWidth: 15
-};
+}];
 
 export default {
     title: 'Charts/Alluvial chart',
