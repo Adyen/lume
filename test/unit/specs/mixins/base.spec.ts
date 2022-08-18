2dcdf267356b4402ec65cf91694278bcd6922c60
Lucas van Heerikhuizen <lucas.vanheerikhuizen@adyen.com>
2022-08-18 17:06:06 +0200

    ✅ improved tests for base.ts mixin to arrive at 100% coverage

--- test/unit/specs/mixins/base.spec.ts
+++ test/unit/specs/mixins/base.spec.ts
@@ -1,5 +1,5 @@
 import { mount } from '@vue/test-utils';
-import { useBase } from '@/mixins/base';
+import { withBase, useBase } from '@/mixins/base';
 import { Orientation } from '@/constants';
 import { data, labels } from '../mock-data';
 import { ref } from '@vue/composition-api';
@@ -18,35 +18,79 @@ const getMixin = (orientation: Orientation = 'horizontal') => {
 }
 
 describe('base.ts', () => {
-    test('should return expected object', async () => {
-        const mixin = getMixin();
+    describe('getMixin', () => {
+        test('should return expected object', async () => {
+            const mixin = getMixin();
 
-        expect(mixin).toBeTruthy();
-        expect(mixin).toHaveProperty('computedData');
-        expect(mixin.computedData.value).toHaveProperty('values');
-        expect(mixin.computedData.value).toEqual(data);
-        expect(mixin).toHaveProperty('containerSize');
-        expect(mixin).toHaveProperty('domain');
-        expect(mixin).toHaveProperty('isHorizontal');
-        expect(mixin).toHaveProperty('updateSize');
+            expect(mixin).toBeTruthy();
+            expect(mixin).toHaveProperty('computedData');
+            expect(mixin.computedData.value).toHaveProperty('values');
+            expect(mixin.computedData.value).toEqual(data);
+            expect(mixin).toHaveProperty('containerSize');
+            expect(mixin).toHaveProperty('updateSize');
+        });
+
+        test('should change width and height of containerSize with vertical orientation', () => {
+            const containerSize = { width: 123, height: 234 }
+            const mixin = getMixin('vertical');
+
+            expect(mixin.containerSize).not.toEqual(containerSize)
+            mixin.updateSize(containerSize)
+            expect(mixin.containerSize).toEqual(containerSize)
+        })
+
+        test('should change width only of containerSize with horizontal orientation', () => {
+            const containerSize = { width: 123, height: 234 }
+            const mixin = getMixin();
+
+            expect(mixin.containerSize).not.toEqual(containerSize)
+            mixin.updateSize(containerSize)
+            expect(mixin.containerSize.width).toEqual(containerSize.width);
+            expect(mixin.containerSize.height).not.toEqual(containerSize.height);
+        });
     });
 
-    test('should change width and height of containerSize with vertical orientation', () => {
-        const containerSize = { width: 123, height: 234 }
-        const mixin = getMixin('vertical');
+    describe('withBase', () => {
+        test('should not have labels as a required property', () => {
+            let mixin = null;
 
-        expect(mixin.containerSize).not.toEqual(containerSize)
-        mixin.updateSize(containerSize)
-        expect(mixin.containerSize).toEqual(containerSize)
-    })
+            const wrapper = mount({
+                template: '<div></div>',
+                props: {
+                    ...withBase(null, false)
+                },
+                setup() {
+                    mixin = useBase(ref(data));
+                    return mixin;
+                }
+            }, { propsData: { data }});
 
-    test('should change width only of containerSize with horizontal orientation', () => {
-        const containerSize = { width: 123, height: 234 }
-        const mixin = getMixin();
+            expect(wrapper.vm.$props.labels).toEqual(null);
+        });
 
-        expect(mixin.containerSize).not.toEqual(containerSize)
-        mixin.updateSize(containerSize)
-        expect(mixin.containerSize.width).toEqual(containerSize.width);
-        expect(mixin.containerSize.height).not.toEqual(containerSize.height);
-    })
+        test('should have labels as a required property', () => {
+            const component = {
+                template: '<div></div>',
+                props: {
+                    ...withBase(null, true)
+                },
+                // Validation for our data set will fail
+            };
+
+            expect(component.props.labels.required).toBe(true)
+        })
+
+        test('should run custom validator on prop', () => {
+            const component = {
+                template: '<div></div>',
+                props: {
+                    ...withBase(data => !!data && data[0].values[0] === 123, false)
+                },
+                // Validation for our data set will fail
+            };
+
+            expect(component.props.data.validator([{ values: [123]}])).toBe(true);
+            expect(component.props.data.validator([{ values: [321]}])).toBe(false);
+        })
+    });
 })
