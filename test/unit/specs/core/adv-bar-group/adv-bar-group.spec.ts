8e055519b242f14c25f1426d69dadf53d830c421
Lucas van Heerikhuizen <lucas.vanheerikhuizen@adyen.com>
2022-08-19 14:44:18 +0200

    ✅ Refined test for bar group to include orientation checks and default color initiation

--- test/unit/specs/core/adv-bar-group/adv-bar-group.spec.ts
+++ test/unit/specs/core/adv-bar-group/adv-bar-group.spec.ts
@@ -1,6 +1,9 @@
 import { mount } from '@vue/test-utils';
 import AdvBarGroup from '@/core/adv-bar-group/adv-bar-group.vue';
 import { data, xScale, yScale } from '../../mock-data';
+import { Orientation } from '@/constants';
+
+const orientation: Orientation = 'horizontal';
 
 describe('bar-group.vue', () => {
     test('mounts component', () => {
@@ -14,5 +17,22 @@ describe('bar-group.vue', () => {
 
         const el = wrapper.find('[data-j-bars-group]');
         expect(el.exists()).toBeTruthy()
+        expect(wrapper.find('[data-j-adv-bar]').attributes('x')).not.toEqual('0');
+        expect(wrapper.find('[data-j-adv-bar]').attributes()['class'].includes('adv-fill-color--01')).toBe(true);
     });
+
+    test('mounts component with horizontal orientation', () => {
+        const wrapper = mount(AdvBarGroup, {
+            propsData: {
+                data,
+                xScale: yScale,
+                yScale: xScale,
+                orientation
+            }
+        })
+
+        const el = wrapper.find('[data-j-bars-group]');
+        expect(el.exists()).toBeTruthy()
+        expect(wrapper.find('[data-j-adv-bar]').attributes('x')).toEqual('0');
+    })
 });
