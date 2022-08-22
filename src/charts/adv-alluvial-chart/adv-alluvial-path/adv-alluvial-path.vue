3677ebe333fb9d488de64f154f090c02a78c953b
govind <govind.srinidhi@adyen.com>
2022-08-22 15:27:36 +0200

    🎨 Alluvial link path made generic so that it can be used for actual and ghost links

---
+++ src/charts/adv-alluvial-chart/adv-alluvial-path/adv-alluvial-path.vue
@@ -0,0 +1,52 @@
+<template>
+  <g>
+    <g
+      v-for="(linkPath, index) in linkPaths"
+      :key="`link-path_${index}`"
+      class="adv-alluvial-group__path-group"
+      style="mix-blend-mode: multiply"
+    >
+      <path
+        :id="isGhostPath ? `${linkPath.id}__ghost` : linkPath.id"
+        :d="linkPath.d"
+        :class="
+          isGhostPath ?
+            ['adv-alluvial-group__path--ghost', 'adv-stroke-color--transparent'] :
+            [`adv-stroke-color--${linkPath.color}`, `adv-alluvial-group__path--${linkPath.color}`, 'adv-alluvial-group__path']
+        "
+        :stroke-width="linkPath.strokeWidth + ghostStrokeWidthOffset"
+        @mouseover="$emit('mouseover', linkPath.link)"
+        @mouseout="$emit('mouseout')"
+      />
+    </g>
+  </g>
+</template>
+
+<script lang="ts">
+
+import { LinkPath } from '@/types/alluvial';
+import { defineComponent, PropType } from '@vue/composition-api';
+import { ghostStrokeWidthOffset } from '../defaults';
+
+export default defineComponent({
+  props: {
+    linkPaths: {
+      type: Array as PropType<Array<LinkPath>>,
+      default: () => []
+    },
+    isGhostPath: {
+      type: Boolean,
+      default: false
+    }
+  },
+  setup(props) {
+    return {
+      ghostStrokeWidthOffset: props.isGhostPath ? ghostStrokeWidthOffset : 0
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+@use './styles';
+</style>
