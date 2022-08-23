68430cf2dae1388561b5720a82a1629b6353cdcb
Lucas van Heerikhuizen <lucas.vanheerikhuizen@adyen.com>
2022-08-23 16:39:04 +0200

    ✅ Added tests for the observe/unobserve hooks being called for the resize mixin

--- test/unit/specs/mixins/resize.spec.ts
+++ test/unit/specs/mixins/resize.spec.ts
@@ -1,27 +1,56 @@
-import { mount } from '@vue/test-utils';
+import { shallowMount } from '@vue/test-utils';
 import { useResizeObserver } from '@/mixins/resize';
-import Vue from 'vue';
 
-const getResizeMixin = () => {
+const getResizeMixin = children => {
     let mixin = null;
-    const wrapper = mount({
-        template: '<div ref="resizeRef" data-j-resize-root></div>',
+    const wrapper = shallowMount({
         setup() {
             mixin = useResizeObserver();
             return mixin;
-        }
+        },
+        template: `<div>${children}</div>`,
     });
 
     return { wrapper, mixin };
 }
 
 describe('resize.ts', () => {
+    const spy = jest.fn();
+
+    beforeAll(() => {
+        class ResizeObserver {
+            observe(args) {
+                spy('observe')
+            }
+            unobserve() {
+                spy('unobserve')
+            }
+            disconnect() {
+                // Do nothing
+            }
+        }
+
+        window.ResizeObserver = ResizeObserver;
+    });
+
     test('should return expected object state and ref', async () => {
         const expected = { dimensions: { bottom: 0, height: 0, left: 0, right: 0, top: 0, width: 0, x: 0, y: 0 }};
-        const { wrapper, mixin } = await getResizeMixin();
+        const { wrapper, mixin } = await getResizeMixin('<div ref="resizeRef" data-j-resize-root></div>');
 
-        const el = wrapper.element;
+        const el = wrapper.find('[data-j-resize-root]').element;
         expect(mixin.resizeRef.value).toEqual(el);
         expect(mixin.resizeState).toEqual(expected);
+        expect(spy).toHaveBeenCalledWith('observe');
+        expect(spy).not.toHaveBeenCalledWith('unobserve');
+
+        await window.dispatchEvent(new Event('resize'));
+
+    });
+
+    test('should call the unobserve method when component is being destroyed', async () => {
+        const { wrapper } = await getResizeMixin('<div ref="resizeRef" data-j-resize-root></div>');
+
+        wrapper.destroy();
+        expect(spy).toHaveBeenCalledWith('unobserve');
     });
 });
