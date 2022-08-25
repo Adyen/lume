6e645103f1e36888f6e4755ffb6619df672a4042
govind <govind.srinidhi@adyen.com>
2022-08-25 10:36:18 +0200

    🔨 Updated stories in alluvial chart

--- src/charts/adv-alluvial-chart/adv-alluvial-chart.stories.ts
+++ src/charts/adv-alluvial-chart/adv-alluvial-chart.stories.ts
@@ -1,71 +1,83 @@
 import { withSizeArgs, withSizeArgTypes } from '@/utils/storybook-helpers';
-import AlluvialChart from './adv-alluvial-chart.vue';
+import AdvAlluvialChart from './adv-alluvial-chart.vue';
 
-const alluvial = [{
-    values: [
-        {
-            label: 'A',
-            color: '01',
-            value: 'A',
-            targets: [
-                { node: 'D', value: 15 },
-                { node: 'E', value: 42 },
-            ],
-        },
-        {
-            label: 'B',
-            color: '02',
-            value: 'B',
-            targets: [
-                { node: 'D', value: 45 },
-                { node: 'E', value: 42 },
-            ],
-        },
-        {
-            label: 'C',
-            color: '03',
-            value: 'C',
-            targets: [
-                { node: 'D', value: 20 },
-            ],
-        },
-        {
-            label: 'D',
-            value: 'D',
-            color: '07',
-        },
-        {
-            label: 'E',
-            value: 'E',
-            color: '06',
-        },
-    ],
-    nodePadding: 20,
-    nodeWidth: 15
-}];
+const alluvial = {
+    data: [{
+        values: [
+            {
+                label: 'A',
+                color: '01',
+                value: 'A',
+                targets: [
+                    {node: 'D', value: 1},
+                    {node: 'E', value: 42},
+                    {node: 'C', value: 3}
+                ],
+            },
+            {
+                label: 'B',
+                color: '02',
+                value: 'B',
+                targets: [
+                    {node: 'D', value: 8},
+                    {node: 'E', value: 1},
+                    {node: 'C', value: 1}
+                ],
+            },
+            {
+                label: 'C',
+                color: '03',
+                value: 'C',
+                targets: [
+                    {node: 'D', value: 12},
+                    {node: 'E', value: 1}
+                ],
+            },
+            {
+                label: 'D',
+                value: 'D',
+                color: '07',
+            },
+            {
+                label: 'E',
+                value: 'E',
+                color: '06',
+            },
+        ],
+        nodePadding: 20,
+        nodeWidth: 15
+    }]
+};
 
 export default {
     title: 'Charts/Alluvial chart',
-    component: AlluvialChart,
+    component: AdvAlluvialChart,
     argTypes: {
         ...withSizeArgTypes(),
+        data: {
+            control: 'object',
+            description: 'Chart data.',
+        },
     },
     args: {
         ...withSizeArgs(),
+        options: {},
     },
 };
 
 export const Basic = ({ argTypes }) => {
     return {
-        components: { AlluvialChart },
+        components: { AdvAlluvialChart },
         props: Object.keys(argTypes),
-        data: () => ({
-            alluvial
-        }),
+        setup(props) {
+            return { props };
+        },
         template: `
     <div :style="{ width: width + 'px', height: height + 'px' }">
-      <alluvial-chart :data="alluvial" />
+      <adv-alluvial-chart v-bind="props" />
     </div>
   `,
     };
 };
+
+Basic.args = alluvial;
