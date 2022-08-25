3d8cde7e3455ae0306dd414be91c899d10431e1a
Lucas van Heerikhuizen <lucas.vanheerikhuizen@adyen.com>
2022-08-25 17:00:51 +0200

    🔥 Killing redundant import

--- test/unit/specs/core/adv-chart-legend/adv-chart-legend.spec.ts
+++ test/unit/specs/core/adv-chart-legend/adv-chart-legend.spec.ts
@@ -1,4 +1,4 @@
-import { shallowMount, mount } from '@vue/test-utils';
+import { mount } from '@vue/test-utils';
 import AdvLegend from '@/core/adv-chart-legend';
 import { generateData } from '../../mock-data';
 
