07be4b8600d29c8d4e69e9c906e844e1214a1791
joaosa <joao.dossantos@adyen.com>
2022-09-01 13:04:56 +0100

    ⚰️ Remove leftover code

--- public/js/src/main.ts
+++ public/js/src/main.ts
@@ -1,11 +1,6 @@
 import Vue from 'vue';
-
 import App from './components/app.vue';
 import '@/styles/main.scss';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const app = new Vue(App).$mount('#root');
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-// const vueApp = createApp(App);
-// vueApp.mount('#root');
