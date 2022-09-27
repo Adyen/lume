46c213adb8f5fdf2e05af01b7ca00f46b60a4096
joaosa <joao.dossantos@adyen.com>
2022-09-27 15:27:40 +0200

    📝 Update sparkline Storybook data

--- src/docs/storybook-data/sparkline-data.ts
+++ src/docs/storybook-data/sparkline-data.ts
@@ -3,8 +3,7 @@ const DATASETS = {
     data: [
       {
         values: [12, 14, 8, 10, 20, 9, 8, 4, 12, 0, 15, 8, 9, 10],
-        color: '02',
-        legend: 'Trend',
+        label: 'Cat adoptions in Dec 2022',
       },
     ],
   },
@@ -13,7 +12,7 @@ const DATASETS = {
       {
         values: [10, 15, 8, -4, -1, 4, 20],
         color: '09',
-        legend: 'Trend',
+        label: 'Member count',
       },
     ],
   },
@@ -21,8 +20,8 @@ const DATASETS = {
     data: [
       {
         values: [30, null, 40, 50, null, null, 60, 20, 10, null],
-        color: '03',
-        legend: 'Trend',
+        color: 'violet',
+        label: 'Toyota vehicles purchased (M)',
       },
     ],
   },
