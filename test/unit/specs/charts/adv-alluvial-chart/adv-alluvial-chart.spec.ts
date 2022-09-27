b46c52d25c518232407c6526272acac01284c5a5
Lucas van Heerikhuizen <lucas.vanheerikhuizen@adyen.com>
2022-09-27 16:55:17 +0200

    ✅ Added tests for cases where there is no data or a single node in the alluvial chart

--- test/unit/specs/charts/adv-alluvial-chart/adv-alluvial-chart.spec.ts
+++ test/unit/specs/charts/adv-alluvial-chart/adv-alluvial-chart.spec.ts
@@ -5,8 +5,22 @@ import { generateData } from './data-generator';
 import { mount } from '@vue/test-utils';
 
 const alluvialChartTestSuiteFactory = (data: any = DATASETS.Basic.data) => new BaseTestSuite(AdvAlluvialChart, { data });
+const [baseData] = generateData();
 
 describe('adv-alluvial-chart.vue', () => {
+  let originalError = null;
+
+  beforeAll(() => {
+    // Suppress error output to the console
+    originalError = console.error;
+    console.error = jest.fn();
+  })
+
+  afterAll(() => {
+    // Restore default error output setting
+    console.error = originalError;
+  })
+
   test('mounts component and sets prop values', async () => {
     
     const testSuite = alluvialChartTestSuiteFactory();
@@ -34,16 +48,11 @@ describe('adv-alluvial-chart.vue', () => {
   });
 
   test('should throw error in case of a dataset with circular links', async () => {
-    // Suppress error output to the console
-    const originalError = console.error;
-    console.error = jest.fn();
-
     const values = [
       { label: 'A', color: '01', value: 'A', targets: [{ node: 'B', value: 50 }] },
       { label: 'B', color: '02', value: 'B', targets: [{ node: 'A', value: 50 }] },
     ];
 
-    const [baseData] = generateData();
     const data = [{
       ...baseData,
       values
@@ -57,13 +66,43 @@ describe('adv-alluvial-chart.vue', () => {
       });
 
     expect(t).toThrow('circular link');
-
-    // Restore default error output setting
-    console.error = originalError;
   });
 
-  // Test to see what happens when we specify empty dataset
-  // Test to see what happens when we specify single node
+  test('should be able to handle an empty dataset', async () => {
+    const values = [];
+
+    const data = [{
+      ...baseData,
+      values
+    }];
+
+    const t = () =>
+      mount(AdvAlluvialChart, {
+        propsData: {
+          data
+        }
+      });
+
+    expect(t).toThrow('Invalid array length');
+  });
+
+  test('should be able to handle an dataset with a single node', async () => {
+    const values = [
+      { label: 'A', color: '01', value: 'A', targets: [] },
+    ];
+
+    const data = [{
+      ...baseData,
+      values
+    }];
+
+    const testSuite = alluvialChartTestSuiteFactory(data);
+
+    const wrapper = testSuite.run().wrapper;
+    const el = wrapper.find('[data-j-alluvial-chart]');
+    expect(el.exists()).toBeTruthy();
+  });
+
   // Test to see what happens when we specify only nodes with no edges
   // Test to see what happens when we specify an edge to a non-existing node
   // Test interaction with elements
