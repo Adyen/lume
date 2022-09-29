51add7f4e440bed4398301610572f28e5fd92dd2
joaosa <joao.dossantos@adyen.com>
2022-09-29 17:05:30 +0200

    🐛 Fix categorical color calculation

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -1,5 +1,6 @@
 import { DataType } from '@/types/dataset';
 import { Color, Colors, DivergentColors, LegacyColors } from '@/constants';
+import { shiftItems } from './helpers';
 
 const DEFAULT_TYPE = DataType.Categorical;
 
@@ -24,8 +25,13 @@ function validateColor(color: Color | null, type: DataType) {
   );
 }
 
-function getCategoricalColor(index: number) {
-  return Object.values(Colors)[index];
+function getCategoricalColor(color: Colors, index: number) {
+  // When computing the categorical color, we need to move items in the colors enum to
+  //   be able to correctly map them to the datasets based on the provided index.
+  // For instance, if the user says the first color should be `royalblue`, then we
+  //   move items in the enum so that index 0 is now `royalblue` (and `skyblue` is last).
+  const colorIndex = Object.values(Colors).indexOf(color);
+  return shiftItems(Object.values(Colors), colorIndex)[index] as Colors;
 }
 
 function getSequentialColor(color: Colors, index: number) {
@@ -42,7 +48,9 @@ function getColorByType(color: Color, type: DataType, index: number) {
   switch (type) {
     default:
     case DataType.Categorical:
-      return isLegacy(color) ? color : getCategoricalColor(index);
+      return isLegacy(color)
+        ? color
+        : getCategoricalColor(color as Colors, index);
     case DataType.Sequential:
       return getSequentialColor(color as Colors, index);
     case DataType.Divergent:
