cce3f00652c0016ec832d62e4e5971b3ecc75376
vivianj <vivian.joseph@adyen.com>
2022-11-18 11:34:18 +0100

    🔧 add vitest config

---
+++ vitest.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig } from 'vitest/config';
+import path from 'path';
+import vue from '@vitejs/plugin-vue2';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    threads: false,
+    root: 'src',
+    setupFiles: ['./test/config/setupTests.ts'],
+    environment: 'happy-dom',
+    coverage: {
+      provider: 'istanbul',
+      reportsDirectory: `./test/coverage`,
+      statements: 80,
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@test': path.resolve(__dirname, './test'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+                  @import "${path.resolve(
+    __dirname,
+    './src/styles/variables'
+  )}";
+              `,
+      },
+    },
+  },
+  plugins: [vue()],
+});
