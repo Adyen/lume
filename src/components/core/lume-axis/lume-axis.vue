3af8d6f2d91f6d59192a428c415cb54a560cb621
vivianj <vivian.joseph@adyen.com>
2022-12-15 15:46:47 +0100

    ✨ add script setup to core components

--- src/components/core/lume-axis/lume-axis.vue
+++ src/components/core/lume-axis/lume-axis.vue
@@ -82,9 +82,23 @@
 </template>
 
 <script lang="ts">
+enum SCALE_MIXIN_MAP {
+  bandScale = 'band-scale-axis',
+  linearScale = 'linear-scale-axis',
+}
+enum POSITIONS {
+  left = 'left',
+  bottom = 'bottom',
+}
+enum TYPES {
+  x = POSITIONS.bottom,
+  y = POSITIONS.left,
+}
+</script>
+
+<script setup lang="ts">
 import {
   computed,
-  defineComponent,
   onMounted,
   PropType,
   reactive,
@@ -113,197 +127,165 @@ import { AxisMixin, AxisMixinFunction } from './types';
 
 import mixinTypes from './composables/';
 
-const SCALE_MIXIN_MAP = {
-  bandScale: 'band-scale-axis',
-  linearScale: 'linear-scale-axis',
-};
-
-const POSITIONS = ['bottom', 'left'];
-
-const TYPES = {
-  x: 'bottom',
-  y: 'left',
-};
-
-export default defineComponent({
-  components: { VuePortal, VuePortalTarget },
-  props: {
-    scale: {
-      type: Function as PropType<Scale>,
-      required: true,
-    },
-    type: {
-      type: String,
-      default: undefined,
-      validator: (value: string) => value in TYPES,
-    },
-    position: {
-      type: String,
-      default: undefined,
-      validator: (value: string) => POSITIONS.includes(value),
-    },
-    containerSize: {
-      type: Object as PropType<ContainerSize>,
-      default: () => ({ width: 0, height: 0 }),
-    },
-    hoveredIndex: {
-      type: Number,
-      default: -1,
-    },
-    orientation: {
-      type: String as PropType<Orientation>,
-      default: ORIENTATIONS.VERTICAL,
-    },
-    ...withOptions<AxisOptions>(),
+const props = defineProps({
+  scale: {
+    type: Function as PropType<Scale>,
+    required: true,
   },
-  setup(props, ctx) {
-    const { scale, containerSize, hoveredIndex, options, orientation } =
+  type: {
+    type: String,
+    default: undefined,
+    validator: (value: string) => value in TYPES,
+  },
+  position: {
+    type: String as PropType<POSITIONS>,
+    default: undefined,
+    validator: (value: string) => value in POSITIONS,
+  },
+  containerSize: {
+    type: Object as PropType<ContainerSize>,
+    default: () => ({ width: 0, height: 0 }),
+  },
+  hoveredIndex: {
+    type: Number,
+    default: -1,
+  },
+  orientation: {
+    type: String as PropType<Orientation>,
+    default: ORIENTATIONS.VERTICAL,
+  },
+  ...withOptions<AxisOptions>(),
+});
+
+const emit = defineEmits<{
+  (e: 'tick-mouseover', value: number): void;
+}>();
+
+const { scale, containerSize, hoveredIndex, options, orientation } =
       toRefs(props); // Needs to be cast as any to avoid it being cast to never by default
 
-    const mixins = reactive<Record<string, AxisMixinFunction>>({});
-    const tickRefs = ref<Array<SVGTextElement>>(null);
-    const root = ref<SVGGElement>(null);
-    const ticksWithAttributes = ref(null);
+const mixins = reactive<Record<string, AxisMixinFunction>>({});
+const tickRefs = ref<Array<SVGTextElement>>(null);
+const root = ref<SVGGElement>(null);
+const ticksWithAttributes = ref(null);
 
-    const { chartID } = useBase();
+const { chartID } = useBase();
 
-    const computedPosition = computed(() =>
-      props.type ? TYPES[props.type] : props.position
-    );
+const computedPosition = computed(() =>
+  props.type ? TYPES[props.type] : props.position
+);
 
-    const computedType = computed(
-      () => props.type || (computedPosition.value === 'left' ? 'y' : 'x')
-    );
+const computedType = computed(
+  () => props.type || (computedPosition.value === 'left' ? 'y' : 'x')
+);
 
-    const shouldHover = computed(
-      () =>
-        (computedType.value === 'x' &&
+const shouldHover = computed(
+  () =>
+    (computedType.value === 'x' &&
           orientation.value === ORIENTATIONS.VERTICAL) ||
         (computedType.value === 'y' &&
           orientation.value === ORIENTATIONS.HORIZONTAL)
-    );
+);
 
-    const { allOptions } = useOptions<AxisOptions>(
-      options,
-      computedType.value === 'x' ? xOptions : yOptions
-    );
+const { allOptions } = useOptions<AxisOptions>(
+  options,
+  computedType.value === 'x' ? xOptions : yOptions
+);
 
-    const { showTick } = useSkip(scale, tickRefs, allOptions.value.skip);
+const { showTick } = useSkip(scale, tickRefs, allOptions.value.skip);
 
-    const axisTransform = computed(
-      () =>
-        `translate(0, ${
-          computedType.value === 'x' ? containerSize.value?.height : 0
-        })`
-    );
+const axisTransform = computed(
+  () =>
+    `translate(0, ${
+      computedType.value === 'x' ? containerSize.value?.height : 0
+    })`
+);
 
-    const ticks = computed(() => {
-      // For band scales, return the full labels array (domain)
-      if ((scale.value as ScaleBand<string | number>).step) {
-        return scale.value.domain();
-      }
+const ticks = computed(() => {
+  // For band scales, return the full labels array (domain)
+  if ((scale.value as ScaleBand<string | number>).step) {
+    return scale.value.domain();
+  }
 
-      const { tickCount } = allOptions.value;
-      const [start, end] = scale.value.domain() as number[];
+  const { tickCount } = allOptions.value;
+  const [start, end] = scale.value.domain() as number[];
 
-      return d3TickGenerator(start, end, tickCount);
-    });
+  return d3TickGenerator(start, end, tickCount);
+});
 
-    const tickFormatter = computed(() => {
-      const { tickFormat } = allOptions.value;
-      return useFormat(tickFormat);
-    });
+const tickFormatter = computed(() => {
+  const { tickFormat } = allOptions.value;
+  return useFormat(tickFormat);
+});
 
-    function formatTick(tick: number | string) {
-      const { showTicks } = allOptions.value;
+function formatTick(tick: number | string) {
+  const { showTicks } = allOptions.value;
 
-      // Hides ticks without hiding `gridLines`
-      if (showTicks === false) return '';
+  // Hides ticks without hiding `gridLines`
+  if (showTicks === false) return '';
 
-      return tickFormatter.value(tick);
-    }
+  return tickFormatter.value(tick);
+}
 
-    function onTickMouseover(index: number) {
-      shouldHover.value && ctx.emit('tick-mouseover', index);
-    }
+function onTickMouseover(index: number) {
+  shouldHover.value && emit('tick-mouseover', index);
+}
 
-    function isHovering(index: number) {
-      return (
-        allOptions.value.withHover &&
+function isHovering(index: number) {
+  return (
+    allOptions.value.withHover &&
         shouldHover.value &&
         hoveredIndex.value === index
-      );
-    }
+  );
+}
 
-    function getTickId(index: number) {
-      return `${chartID}-${computedType.value}-tick--${index}`;
-    }
+function getTickId(index: number) {
+  return `${chartID}-${computedType.value}-tick--${index}`;
+}
 
-    // Needed because tick element index changes in the `tickRefs` array
-    function getTextNode(index: number) {
-      if (!tickRefs.value) return;
-      return tickRefs.value.find((tick) => +tick.dataset.index === index);
-    }
+// Needed because tick element index changes in the `tickRefs` array
+function getTextNode(index: number) {
+  if (!tickRefs.value) return;
+  return tickRefs.value.find((tick) => +tick.dataset.index === index);
+}
 
-    function setTicks() {
-      ticksWithAttributes.value = ticks.value.map((tick, index) => {
-        return {
-          value: tick,
-          group: mixins.getTickGroupAttributes(tick),
-          ghost: mixins.getTickGhostAttributes(getTextNode(index)),
-          label: mixins.getTickLabelAttributes(),
-          gridLine: mixins.getGridLinesAttributes(),
-        };
-      });
-    }
+function setTicks() {
+  ticksWithAttributes.value = ticks.value.map((tick, index) => {
+    return {
+      value: tick,
+      group: mixins.getTickGroupAttributes(tick),
+      ghost: mixins.getTickGhostAttributes(getTextNode(index)),
+      label: mixins.getTickLabelAttributes(),
+      gridLine: mixins.getGridLinesAttributes(),
+    };
+  });
+}
 
-    function init() {
-      const scaleType = (scale.value as ScaleBand<string | number>).step
-        ? 'bandScale'
-        : 'linearScale';
+function init() {
+  const scaleType = (scale.value as ScaleBand<string | number>).step
+    ? 'bandScale'
+    : 'linearScale';
 
-      // Get mixin generator based on the scale type
-      const mixin: AxisMixin =
+  // Get mixin generator based on the scale type
+  const mixin: AxisMixin =
         mixinTypes[`${computedType.value}-${SCALE_MIXIN_MAP[scaleType]}`];
 
-      // Push all mixin functions into the `mixins` reactive object
-      Object.entries(mixin(scale, containerSize, allOptions) || []).forEach(
-        ([fnName, fn]) => {
-          mixins[fnName] = fn;
-        }
-      );
+  // Push all mixin functions into the `mixins` reactive object
+  Object.entries(mixin(scale, containerSize, allOptions) || []).forEach(
+    ([fnName, fn]) => {
+      mixins[fnName] = fn;
     }
+  );
+}
 
-    // Setup watcher to get new mixins if scale changes (i.e. vertical to horizontal)
-    watch(scale, init, { flush: 'sync', immediate: true });
+// Setup watcher to get new mixins if scale changes (i.e. vertical to horizontal)
+watch(scale, init, { flush: 'sync', immediate: true });
 
-    // Re-render after `tickRefs` is defined (to grab text width)
-    // and only when `ticks` change (if scale changes)
-    watch([ticks, tickRefs], setTicks, { immediate: true });
+// Re-render after `tickRefs` is defined (to grab text width)
+// and only when `ticks` change (if scale changes)
+watch([ticks, tickRefs], setTicks, { immediate: true });
 
-    onMounted(() => svgCheck(root.value));
-
-    return {
-      allOptions,
-      axisTransform,
-      chartID,
-      computedPosition,
-      computedType,
-      formatTick,
-      getTextNode,
-      getTickId,
-      isHovering,
-      mixins,
-      onTickMouseover,
-      root,
-      shouldHover,
-      showTick,
-      tickRefs,
-      ticks,
-      ticksWithAttributes,
-    };
-  },
-});
+onMounted(() => svgCheck(root.value));
 </script>
 
 <style lang="scss" scoped>
