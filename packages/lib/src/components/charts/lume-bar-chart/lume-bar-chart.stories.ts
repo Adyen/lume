7bc63f8e8a22dceeb7911aac66498e7d505e8546
joaosa <joao.dossantos@adyen.com>
2023-08-31 14:30:24 +0200

    ♻️ Refactor Bar chart stories; Fix related issues

--- packages/lib/src/components/charts/lume-bar-chart/lume-bar-chart.stories.ts
+++ packages/lib/src/components/charts/lume-bar-chart/lume-bar-chart.stories.ts
@@ -1,4 +1,5 @@
 import { computed } from 'vue';
+import type { Meta, StoryObj } from '@storybook/vue3';
 
 import {
   actionEventHandlerTemplate,
@@ -13,12 +14,7 @@ import LumeTooltip from '../../core/lume-tooltip/index';
 
 import { Colors, ORIENTATIONS } from '@/utils/constants';
 
-// const tickFormat = {
-//   'charts-bar-chart--maximum-datasets': '.2s',
-//   'charts-bar-chart--real-data': '~s',
-// };
-
-export default {
+const meta: Meta<typeof LumeBarChart> = {
   title: 'Charts/Bar chart',
   component: LumeBarChart,
   argTypes: {
@@ -54,121 +50,159 @@ export default {
   },
   args: {
     ...withSizeArgs(),
-    options: {},
-    title: 'Bar chart',
   },
 };
 
-const Template = ({ args }) => ({
-  components: { LumeBarChart },
-  setup() {
-    const computedColor = computed(() => Colors[args.color]);
-    return { args, computedColor };
-  },
-  methods: { captureAction },
-  template: `
-  <div :style="{ width: args.width + 'px', height: args.orientation !== 'horizontal' ? args.height + 'px' : undefined }">
-      <lume-bar-chart v-bind="args" :color="computedColor" ${actionEventHandlerTemplate} />
-  </div>
-  `,
-});
+export default meta;
 
-const CustomTemplate = ({ args }) => ({
-  components: { LumeBarChart, LumeTooltip },
-  setup() {
-    const computedColor = computed(() => Colors[args.color]);
-    const customItemsFunction = (data, hoveredIndex) => {
-      if (hoveredIndex > -1) {
-        const { color, label, values } = data[0];
-        return [
-          {
-            color,
-            label,
-            value: values[hoveredIndex].value ?? 0,
-          },
-        ];
-      }
-      return [];
-    };
-    return { args, computedColor, customItemsFunction };
-  },
-  template: `
-  <div :style="{ width: args.width + 'px', height: args.orientation !== 'horizontal' ? args.height + 'px' : undefined }">
-      <lume-bar-chart v-bind="args" :color="computedColor">
-        <template #tooltip = "{ data, hoveredIndex, targetElement }">
-          <lume-tooltip :items="customItemsFunction(data, hoveredIndex)" :target-element="targetElement" position="top"/>
-        </template>
-      </lume-bar-chart>
-  </div>
-  `,
-});
+type Story = StoryObj<typeof LumeBarChart>;
 
-export const VerticalBarChart = Template.bind({});
-VerticalBarChart.args = {
-  ...DATASETS.Single,
-  orientation: ORIENTATIONS.VERTICAL,
-  options: {
-    xAxisOptions: {
-      skip: 2,
+export const VerticalBarChart: Story = {
+  render: ({ args }) => ({
+    components: { LumeBarChart },
+    setup() {
+      const computedColor = computed(() => Colors[args.color]);
+      return { args, computedColor, captureAction };
     },
-    yAxisOptions: {},
-  },
-};
-
-export const HorizontalBarChart = Template.bind({});
-HorizontalBarChart.args = {
-  ...DATASETS.LifeExpectancy,
-  orientation: ORIENTATIONS.HORIZONTAL,
-  options: {
-    xAxisOptions: {},
-    yAxisOptions: {},
-  },
-};
-
-export const MissingDataPoints = Template.bind({});
-MissingDataPoints.args = {
-  ...DATASETS.MissingDataPoints,
-  orientation: ORIENTATIONS.VERTICAL,
-  options: {
-    xAxisOptions: {},
-    yAxisOptions: {},
-    tooltipOptions: {
-      valueFormat: (value) => (value !== 'No data' ? `EUR ${value}` : value),
+    template: `<div :style="{ width: args.width + 'px', height: args.orientation !== 'horizontal' ? args.height + 'px' : undefined }">
+        <lume-bar-chart v-bind="args" :color="computedColor" ${actionEventHandlerTemplate} />
+      </div>`,
+  }),
+  args: {
+    ...DATASETS.Single,
+    orientation: ORIENTATIONS.VERTICAL,
+    options: {
+      xAxisOptions: {
+        skip: 1,
+      },
     },
-    withLegend: false,
-  },
-  title: 'Money spent on cats',
-};
-
-export const GroupedBarChart = Template.bind({});
-GroupedBarChart.args = {
-  ...DATASETS.AdoptedAnimals,
-  type: 'grouped',
-  orientation: ORIENTATIONS.VERTICAL,
-  options: {
-    xAxisOptions: {},
-    yAxisOptions: {},
   },
 };
 
-export const StackedBarChart = Template.bind({});
-StackedBarChart.args = {
-  ...DATASETS.WeeklyAnimalEncounter,
-  type: 'stacked',
-  orientation: ORIENTATIONS.VERTICAL,
-};
-
-export const CustomTooltip = CustomTemplate.bind({});
-CustomTooltip.args = {
-  ...DATASETS.LifeExpectancy,
-  orientation: ORIENTATIONS.VERTICAL,
-  options: {
-    xAxisOptions: {},
-    yAxisOptions: {},
+export const HorizontalBarChart: Story = {
+  render: ({ args }) => ({
+    components: { LumeBarChart },
+    setup() {
+      const computedColor = computed(() => Colors[args.color]);
+      return { args, computedColor, captureAction };
+    },
+    template: `<div :style="{ width: args.width + 'px', height: args.orientation !== 'horizontal' ? args.height + 'px' : undefined }">
+        <lume-bar-chart v-bind="args" :color="computedColor" ${actionEventHandlerTemplate} />
+      </div>`,
+  }),
+  args: {
+    ...DATASETS.LifeExpectancy,
+    orientation: ORIENTATIONS.HORIZONTAL,
   },
 };
 
-export const Empty = Template.bind({});
-Empty.args = {
-  ...DATASETS.Empty,
+export const MissingDataPoints: Story = {
+  render: ({ args }) => ({
+    components: { LumeBarChart },
+    setup() {
+      const computedColor = computed(() => Colors[args.color]);
+      return { args, computedColor, captureAction };
+    },
+    template: `<div :style="{ width: args.width + 'px', height: args.orientation !== 'horizontal' ? args.height + 'px' : undefined }">
+        <lume-bar-chart v-bind="args" :color="computedColor" ${actionEventHandlerTemplate} />
+      </div>`,
+  }),
+  args: {
+    ...DATASETS.MissingDataPoints,
+    orientation: ORIENTATIONS.VERTICAL,
+    options: {
+      tooltipOptions: {
+        valueFormat: (value) => (value !== 'No data' ? `EUR ${value}` : value),
+      },
+      withLegend: false,
+    },
+    title: 'Money spent on cats',
+  },
+};
+
+export const GroupedBarChart: Story = {
+  render: ({ args }) => ({
+    components: { LumeBarChart },
+    setup() {
+      const computedColor = computed(() => Colors[args.color]);
+      return { args, computedColor, captureAction };
+    },
+    template: `<div :style="{ width: args.width + 'px', height: args.orientation !== 'horizontal' ? args.height + 'px' : undefined }">
+        <lume-bar-chart v-bind="args" :color="computedColor" ${actionEventHandlerTemplate} />
+      </div>`,
+  }),
+  args: {
+    ...DATASETS.AdoptedAnimals,
+    type: 'grouped',
+    orientation: ORIENTATIONS.VERTICAL,
+  },
+};
+
+export const StackedBarChart: Story = {
+  render: ({ args }) => ({
+    components: { LumeBarChart },
+    setup() {
+      const computedColor = computed(() => Colors[args.color]);
+      return { args, computedColor, captureAction };
+    },
+    template: `<div :style="{ width: args.width + 'px', height: args.orientation !== 'horizontal' ? args.height + 'px' : undefined }">
+        <lume-bar-chart v-bind="args" :color="computedColor" ${actionEventHandlerTemplate} />
+      </div>`,
+  }),
+  args: {
+    ...DATASETS.WeeklyAnimalEncounter,
+    type: 'stacked',
+    orientation: ORIENTATIONS.VERTICAL,
+  },
+};
+
+export const CustomTooltip: Story = {
+  render: ({ args }) => ({
+    components: { LumeBarChart, LumeTooltip },
+    setup() {
+      const computedColor = computed(() => Colors[args.color]);
+      const customItemsFunction = (data, hoveredIndex) => {
+        if (hoveredIndex > -1) {
+          const { color, label, values } = data[0];
+          return [
+            {
+              color,
+              label,
+              value: `${values[hoveredIndex].value ?? 0} years`,
+            },
+          ];
+        }
+        return [];
+      };
+      return { args, computedColor, customItemsFunction };
+    },
+    template: `
+    <div :style="{ width: args.width + 'px', height: args.orientation !== 'horizontal' ? args.height + 'px' : undefined }">
+        <lume-bar-chart v-bind="args" :color="computedColor">
+          <template #tooltip = "{ data, hoveredIndex, targetElement }">
+            <lume-tooltip :items="customItemsFunction(data, hoveredIndex)" :target-element="targetElement" position="top"/>
+          </template>
+        </lume-bar-chart>
+    </div>
+    `,
+  }),
+  args: {
+    ...DATASETS.LifeExpectancy,
+    orientation: ORIENTATIONS.VERTICAL,
+  },
+};
+
+export const Empty: Story = {
+  render: ({ args }) => ({
+    components: { LumeBarChart },
+    setup() {
+      return { args, captureAction };
+    },
+    template: `<div :style="{ width: args.width + 'px', height: args.orientation !== 'horizontal' ? args.height + 'px' : undefined }">
+        <lume-bar-chart v-bind="args" ${actionEventHandlerTemplate}  />
+      </div>`,
+  }),
+  args: {
+    ...DATASETS.Empty,
+  },
 };
