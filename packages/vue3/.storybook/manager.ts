8f20d1b2707b0312807dfbb67d411b167499ab93
joaosa <joao.dossantos@adyen.com>
2023-09-26 11:57:06 +0200

    🩹 Fix Storybook sidebar logo

--- packages/vue3/.storybook/manager.ts
+++ packages/vue3/.storybook/manager.ts
@@ -2,10 +2,11 @@ import { addons } from '@storybook/manager-api';
 import { create } from '@storybook/theming';
 
 import themeObject from 'storybook-utils/theme';
+import brandImage from 'storybook-utils/assets/logo.svg';
 
 const theme = create({
   ...themeObject,
-  brandImage: '../node_modules/storybook-utils/assets/logo.svg',
+  brandImage,
 });
 
 addons.setConfig({
