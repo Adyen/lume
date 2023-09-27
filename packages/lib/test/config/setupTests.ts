fcb41009ace06c944f01b8404b1524b211ee1f61
govind <govind.srinidhi@adyen.com>
2023-09-27 13:21:51 +0200

    🐛 Fixed the tooltip jitter on initial hover

--- packages/lib/test/config/setupTests.ts
+++ packages/lib/test/config/setupTests.ts
@@ -1,5 +1,22 @@
 // Jest cannot find these functionalities natively, so we pad them in as best we need to
 global.ResizeObserver = require('resize-observer-polyfill');
 global.structuredClone = (object) => JSON.parse(JSON.stringify(object));
+global.retry = (assertion, { interval = 20, timeout = 1000 } = {}) => {
+  return new Promise((resolve, reject) => {
+    const startTime = Date.now();
+
+    const tryAgain = () => {
+      setTimeout(() => {
+        try {
+          resolve(assertion());
+        } catch (err) {
+          Date.now() - startTime > timeout ? reject(err) : tryAgain();
+        }
+      }, interval);
+    };
+
+    tryAgain();
+  });
+};
 
 export {};
