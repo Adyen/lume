0a06efe33ff2e40ba38083dc5b5ccbc93bfbc7e8
joaosa <joao.dossantos@adyen.com>
2024-01-09 11:35:34 +0100

    🐛 Fix handling datasets with empty values

--- packages/lib/src/utils/errors.ts
+++ packages/lib/src/utils/errors.ts
@@ -3,11 +3,15 @@ import { isProd } from './helpers';
 export enum Errors {
   // Alluvial
   GraphProblem = 'There was an issue generating your alluvial graph.',
+
+  // Bar
+  BarScaleDomainAbove0 = 'Bar linear scale domain cannot start above 0!',
+  BarScaleNull = 'Bar linear scale cannot be `null`',
 }
 
 const PREFIX = '[lume] ';
 
-export function error(error: Errors, stack: Error, devOnly = true) {
+export function error(error: Errors, stack?: Error, devOnly = true) {
   if (devOnly && isProd()) return;
   console.error(PREFIX + 'Error: ' + error, stack);
 }
